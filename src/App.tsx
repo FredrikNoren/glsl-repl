d9eda648231d995dfe65ec7c05b6dd64cf429c24
Fredrik Norén <fredrik.jw.noren@gmail.com>
2019-12-09 11:35:00 +0100

    Fix output at bottom

--- src/App.tsx
+++ src/App.tsx
@@ -154,18 +154,21 @@ function App() {
   }, [shader]);
   return (
     <div className="App">
-      <CodeMirror
-        value={shader}
-        onChange={setShader}
-        options={{
-          theme: 'one-dark',
-          mode: 'clike'
-        }}
-        />
+      <div className="CodeMirrorContainer">
+        <CodeMirror
+          value={shader}
+          onChange={setShader}
+          options={{
+            theme: 'one-dark',
+            mode: 'clike',
+            lineNumbers: true
+          }}
+          />
+      </div>
+      <div className="Result">{result}</div>
       <div className="Header">
         glsl-repl by <a href="https://twitter.com/jfnoren">@jfnoren</a>. Code on <a href="https://github.com/FredrikNoren/glsl-repl">GitHub</a>. Find it useful? <a href="https://www.patreon.com/fredriknoren">Become a Patron!</a>
       </div>
-      <div className="Result">{result}</div>
     </div>
   );
 }
